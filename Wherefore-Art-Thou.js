fddd664b305d40d52a711c175880485f6e1dbe5b
Nicole <heeyeons1993@gmail.com>
2019-10-07 11:17:25 -0700

    wrote simple code that looks through an array of objects and returns an array, matching name and value pairs of second argument.

---
+++ Wherefore-Art-Thou.js
@@ -0,0 +1,26 @@
+/*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument).
+Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object
+from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+*/
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function (obj) {
+    return srcKeys.every(function (key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+console.log(whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }));
+
+//1. get a key in the source that we campare it with first argument.
+//2. filter the array with srckeys and get every thing in the object matches with condition which is object had property of key, and it is same as source's key value.
